249712889e36552e2e5272e2246ddbcfd89b3772
Davi-604 <davimedeiroscv100@gmail.com>
2026-07-06 10:46:32 -0300

    style: softens the shadow of the our story home section

--- src/features/home/components/OurStory.tsx
+++ src/features/home/components/OurStory.tsx
@@ -168,8 +168,7 @@ export function OurStory() {
         {
           rotateX: 20,
           scale: startScale,
-          boxShadow:
-            '0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003',
+          boxShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.25), 0 0 15px rgba(0, 0, 0, 0.1)',
         },
         {
           rotateX: 0,
@@ -195,8 +194,7 @@ export function OurStory() {
         {
           rotateX: -20,
           scale: startScale,
-          boxShadow:
-            '0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003',
+          boxShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.25), 0 0 15px rgba(0, 0, 0, 0.1)',
           ease: 'none',
           scrollTrigger: {
             trigger: section,
