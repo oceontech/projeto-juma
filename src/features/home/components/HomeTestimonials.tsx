faf8c57c61b39ee24075a36428fdb9c4307d92ce
Davi-604 <davimedeiroscv100@gmail.com>
2026-07-29 15:27:40 -0300

    style: fix the space between the border cards and the x borders of the invisible container

--- src/features/home/components/HomeTestimonials.tsx
+++ src/features/home/components/HomeTestimonials.tsx
@@ -105,8 +105,8 @@ export function HomeTestimonials() {
         </div>
 
         {/* Cards */}
-        <div 
-          className="flex justify-center gap-6 overflow-hidden h-[500px] md:h-[600px] lg:h-[700px]" 
+        <div
+          className="flex justify-center gap-6 overflow-hidden h-[500px] md:h-[600px] lg:h-[700px] px-12"
           style={{ maskImage: 'linear-gradient(to bottom, transparent, black 10%, black 90%, transparent)', WebkitMaskImage: 'linear-gradient(to bottom, transparent, black 10%, black 90%, transparent)' }}
         >
           <TestimonialsColumn testimonials={testimonialsData} duration={25} className="w-full md:w-1/2 lg:w-1/3" />
